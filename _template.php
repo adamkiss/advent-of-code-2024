e8791d88e31738c796bec2946a04547ebe2e6862
Adam Kiss <iam@adamkiss.com>
2024-12-09 12:34:47 +0100

    Add more bench to template

--- _template.php
+++ _template.php
@@ -16,11 +16,23 @@ PASTEDEMOINPUTHERE
 INPUT;
 
 $s = microtime(true);
-// PART 1
+
+// 1
+$p = microtime(true);
 println('1) Result of demo: ' . part1($demoinput));
+printf("» %.3fms\n", (microtime(true)-$p) * 1000);
+
+$p = microtime(true);
 println('1) Result of real input: ' . part1($input));
-println('–––');
-// PART 2
+printf("» %.3fms\n", (microtime(true)-$p) * 1000);
+
+// 2
+$p = microtime(true);
 println('2) Result of demo: ' . part2($demoinput));
+printf("» %.3fms\n", (microtime(true)-$p) * 1000);
+
+$p = microtime(true);
 println('2) Result of real input: ' . part2($input));
-printf("TOTAL: %.3fms\n", ($s - microtime(true)) * 1000);
+printf("» %.3fms\n", (microtime(true)-$p) * 1000);
+
+printf("TOTAL: %.3fms\n", (microtime(true)-$s) * 1000);
